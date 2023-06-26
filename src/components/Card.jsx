27823b20d08b9894c52456207653f46e48111710
nikitavar22 <aleroev2000@list.ru>
2023-06-26 21:44:38 +0300

    This is project on react

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 import InputComponent from "./InputComponent";
-import MainPage from "./MainPage";
-import {Route, Routes, Link} from "react-router-dom";
 
 function Card(props) {
     const { type, tasks, task, setTasks } = props
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './assets/index.css';
 import App from './App';
 import reportWebVitals from './assets/reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
           <App />
-      </BrowserRouter>
   </React.StrictMode>
 );
 
